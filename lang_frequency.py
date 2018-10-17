43bb45be2eb7fb51e58b8c8f1b991f2e79c4fb1b
olegush <43958767+olegush@users.noreply.github.com>
2018-10-17 14:41:45 +0300

    used Counter class

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,6 +1,7 @@
 import os.path
 import sys
 import string
+import collections
 
 
 def load_data(filepath):
@@ -12,11 +13,8 @@ def get_most_frequent_words(text):
     formatted_text = text.translate(None, '\n\r\r{}'
                                     .format(string.punctuation)).lower()
     formatted_text_list = formatted_text.split(' ')
-    most_frequent_words_dict = {}
-    for word in formatted_text_list:
-        most_frequent_words_dict[word] = formatted_text_list.count(word)
-    return sorted(most_frequent_words_dict,
-                  key=most_frequent_words_dict.get, reverse=True)
+    return collections.Counter(filter(lambda item: item != '',
+                                      formatted_text_list)).most_common(10)
 
 
 if __name__ == '__main__':
@@ -26,8 +24,6 @@ if __name__ == '__main__':
     if not os.path.exists(user_filepath):
         exit('need correct file path')
     user_text = load_data(user_filepath)
-    ten_most_frequent_words = get_most_frequent_words(user_text)[0:10]
-    ten_most_frequent_words.remove('')
     print('The 10 most_frequent_words in the text:')
-    for word in ten_most_frequent_words:
-        print('"{}"'.format(word))
+    for word in get_most_frequent_words(user_text):
+        print('"{}"'.format(word[0]))
