972b8cbda911929130776fb092dd2ff01d269927
olegush <43958767+olegush@users.noreply.github.com>
2018-10-18 22:41:55 +0300

    output removed from function

--- lang_frequency.py
+++ lang_frequency.py
@@ -16,10 +16,7 @@ def get_most_frequent_words(data_txt):
     most_frequent_words_list = collections.Counter(
         filter(lambda item: item != '', formatted_text.split(' '))
     ).most_common(words_quantity)
-    most_frequent_words = ''
-    for word, quantity in most_frequent_words_list:
-        most_frequent_words += '"{}"\n'.format(word)
-    return most_frequent_words
+    return most_frequent_words_list
 
 
 if __name__ == '__main__':
@@ -27,10 +24,9 @@ if __name__ == '__main__':
         user_filepath = sys.argv[1]
         user_data_txt = load_data(user_filepath)
         print('The 10 most_frequent_words in the text:')
-        print(get_most_frequent_words(user_data_txt))
+        for word, quantity in get_most_frequent_words(user_data_txt):
+            print('"{}"'.format(word))
     except IndexError:
         print('No script parameter (path to txt file)')
     except IOError:
         print ('No such file or directory')
-    except ValueError:
-        print('No text in the file)')
