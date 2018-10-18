9eb38840b651fd3de9097b76e150d303a8a67e47
olegush <43958767+olegush@users.noreply.github.com>
2018-10-18 10:05:10 +0300

    fixed bugs, improved the code

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,31 +1,32 @@
-import os.path
 import sys
 import string
 import collections
 
 
 def load_data(filepath):
-    with open(filepath, 'rb') as file:
+    with open(filepath, 'r') as file:
         return file.read()
 
 
-def get_most_frequent_words(text):
+def get_most_frequent_words(data):
     words_quantity = 10
-    formatted_text = text.translate(None, '\n\r\r{}'
-                                    .format(string.punctuation)).lower()
-    formatted_text_list = formatted_text.split(' ')
-    return collections.Counter(
-        filter(lambda item: item != '',
-               formatted_text_list)).most_common(words_quantity)
+    formatted_text = data.translate(
+        None, '\n\r\r{}'.format(string.punctuation)
+    ).lower()
+    formatted_list = filter(lambda item: item != '', formatted_text.split(' '))
+    return collections.Counter(formatted_list).most_common(words_quantity)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        exit('need parameter')
-    user_filepath = sys.argv[1]
-    if not os.path.exists(user_filepath):
-        exit('need correct file path')
-    user_text = load_data(user_filepath)
-    print('The 10 most_frequent_words in the text:')
-    for word in get_most_frequent_words(user_text):
-        print('"{}"'.format(word[0]))
+    try:
+        user_filepath = sys.argv[1]
+        user_data = load_data(user_filepath)
+        print('The 10 most_frequent_words in the text:')
+        for word, quantity in get_most_frequent_words(user_data):
+            print('"{}"'.format(word))
+    except IndexError:
+        print('No script parameter (path to txt file)')
+    except IOError:
+        print ('No such file or directory')
+    except ValueError:
+        print('No text in the file)')
