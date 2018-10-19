0104a6705b3f3683b2d62f06b45bcdd4556d5955
olegush <43958767+olegush@users.noreply.github.com>
2018-10-19 14:00:00 +0300

    fixed except, added the question

--- lang_frequency.py
+++ lang_frequency.py
@@ -10,6 +10,7 @@ def load_data(filepath):
 
 def get_most_frequent_words(data_txt):
     words_quantity = 10
+    # не понимаю что ломается. все языки перепробовал. поясните
     formatted_text = data_txt.translate(
         None, '\n\r\r{}'.format(string.punctuation)
     ).lower()
@@ -23,10 +24,11 @@ if __name__ == '__main__':
     try:
         user_filepath = sys.argv[1]
         user_data_txt = load_data(user_filepath)
-        print('The 10 most_frequent_words in the text:')
-        for word, quantity in get_most_frequent_words(user_data_txt):
-            print('"{}"'.format(word))
     except IndexError:
         print('No script parameter (path to txt file)')
     except IOError:
-        print ('No such file or directory')
+        print('No such file or directory')
+    else:
+        print('The 10 most_frequent_words in the text:')
+        for word, quantity in get_most_frequent_words(user_data_txt):
+            print('"{}"'.format(word))
